f1b0710ae25c77895594bc050d667a4431568531
Anna Sarabasheva <annasarabasheva@gmail.com>
2025-05-08 13:20:17 +0300

    Add Header Component

--- my-portfolio/src/App.jsx
+++ my-portfolio/src/App.jsx
@@ -1,13 +1,16 @@
 import './App.css'
 import { Routes, Route } from 'react-router-dom';
 import Home from './components/home/Home'
+import Header from './components/header/Header';
 
 
 function App() {
 
   return (
     <>
+      <Header />
       <Routes>
+
         <Route path="/" element={<Home />} />
       </Routes>
     </>
---
+++ my-portfolio/src/components/header/Header.jsx
@@ -0,0 +1,26 @@
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import './Header.css';
+
+const Header = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  return (
+    <header className="header">
+      <button className="menu-toggle" onClick={() => setMenuOpen(!menuOpen)}>
+        ☰
+      </button>
+      <nav>
+        <ul className={`nav ${menuOpen ? 'active' : ''}`}>
+          <li><Link to="/" onClick={() => setMenuOpen(false)}>Home</Link></li>
+          <li><Link to="#education" onClick={() => setMenuOpen(false)}>Education</Link></li>
+          <li><Link to="#experience" onClick={() => setMenuOpen(false)}>Experience</Link></li>
+          <li><Link to="#skills" onClick={() => setMenuOpen(false)}>Skills</Link></li>
+          <li><Link to="#contact" onClick={() => setMenuOpen(false)}>Contact</Link></li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
